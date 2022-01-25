8757da0e3117e8c3c3f1b517b059aa459356a88c
C0MMANDA <jonas0502.jr@gmail.com>
2022-01-25 16:00:04 +0100

    Final Release
    Erste Fertigstellung des Programms

--- src/main.cpp
+++ src/main.cpp
@@ -1,56 +1,65 @@
 #include <Arduino.h>
-int LEDblue = 3;        // Blue on Pin 3
-int LEDred = 5;         // Red on Pin 5
-int LEDgreen = 6;       // Green on Pin 6
-int t = 1000;           // Break 1 sec.
-int brightness1a = 150; // Set blues brightness on a Value
-int brightness1b = 200; // Set rot brightness on a Value
-int brightness1c = 250; // Set green brightness on a Value
-int dark = 0;           // Set brightness on 0
-int sensor;
+int LEDgreen = 3;        // Grün Pin 3
+int LEDred = 5;         // Rot Pin 5
+int LEDblue = 6;       // Blau Pin 6
+int t = 1000;           // Pause 1 sec.
+int brightness1a = 150; // Helligkeit Grün
+int brightness1b = 200; // Helligkeit Rot
+int brightness1c = 250; // Helligkeit Blau 
+int dark = 0;           // Helligkeit ganz unten
+int sensor;             
+int rowCount = 500;
+int rowNumber = 0;
 
 void setup()
 {
-  pinMode(LEDblue, OUTPUT);
   pinMode(LEDgreen, OUTPUT);
+  pinMode(LEDblue, OUTPUT);
   pinMode(LEDred, OUTPUT);
   Serial.begin(9600);
   while (!Serial)
     ;
-  Serial.println("1 for blue, 2 for red, 3 for green");
+  Serial.println("1 für Grün, 2 für Rot, 3 für Blau");
 }
 
 void loop()
 {
   if (Serial.available())
   {
-    analogWrite(LEDblue, dark);
-    analogWrite(LEDred, dark);
     analogWrite(LEDgreen, dark);
+    analogWrite(LEDred, dark);
+    analogWrite(LEDblue, dark);
 
     int state = Serial.parseInt();
     if (state == 1)
     {
-      analogWrite(LEDblue, brightness1a); // blue on
+      analogWrite(LEDgreen, brightness1a); // Grün on
       delay(t);
-      Serial.println("Command completed BlueLED turned ON");
+      Serial.println("Grün an!");
     }
 
     if (state == 2)
     {
-      analogWrite(LEDred, brightness1b); // red on
+      analogWrite(LEDred, brightness1b); // Rot on
       delay(t);
-      Serial.println("Command completed RedLED turned ON");
+      Serial.println("Rot an!");
     }
 
     if (state == 3)
     {
-      analogWrite(LEDgreen, brightness1c); //green on
+      analogWrite(LEDblue, brightness1c); // Blau on
       delay(t);
-      Serial.println("Command completed GreenLED turned ON");
+      Serial.println("Grün an!");
     }
     sensor = analogRead(0);
-    Serial.println(sensor);
+    // Serial.println(sensor);
+
+    if (rowNumber < rowCount)
+    {
+      Serial.print(++rowNumber);
+      Serial.print('\t');
+      Serial.println(sensor); //Sensorwert Ausgabe
+    }
     delay(100);
   }
 }
